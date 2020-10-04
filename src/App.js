37111ac7dac1f792974864661bdcbea1cb30a721
chankruze <chankruze@gmail.com>
2020-10-04 05:51:12 +0530

    Adds Responsive Navbar
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import NavbarResponsive from './components/Navbar/NavbarResponsive'
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavbarResponsive />
     </div>
   )
 }
